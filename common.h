3e39c2024bcf143be18259b1c59630df6c586cb3
renyu <jiaorenyu@163.com>
2014-09-27 21:41:46 +0800

    start from now on.

--- common.h
+++ common.h
@@ -1,6 +1,12 @@
 #ifndef __COMMON_H
 #define __COMMON_H
 
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
 #include <iostream>
+
+#include "const.h"
 
 #endif
--- server.cpp
+++ server.cpp
@@ -2,7 +2,20 @@
 
 int main()
 {
-	std::cout << "Hello World!"  << std::endl;
+	int						listenfd;
+	struct sockaddr_in		serv;
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	bzero(&serv, sizeof(serv));
+	serv.sin_family = AF_INET;
+	serv.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port  = htons(SERV_PORT);	
+
+	
+	std::cout << "starting server..." << std::endl;
+	
+		
 
 	return 0;
 }
